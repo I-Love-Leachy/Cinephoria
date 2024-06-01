55e77381c166689342077de40e99c4a32e6126d1
Leandra Chevrier <leachevrier@hotmail.com>
2024-06-01 19:39:44 +0200

    Footer and Contact page modification

--- server/app.js
+++ server/app.js
@@ -11,11 +11,17 @@ app.set('views', path.join(__dirname, '..', 'client', 'views'));
 
 // Application's routes
 app.get('/',(req, res) =>{
-    res.render('layouts/accueil');
+    res.render('layouts/accueil', {title: "Bienvenue à Cinéphoria."});
 });
 
 app.get('/contact',(req, res) =>{
-    res.render('layouts/contact');
+    res.render('layouts/contact', {title: "Contactez-Nous."});
+    
+})
+
+app.get('/login',(req, res) =>{
+    res.render('layouts/login', {title: "Connectez-Vous."});
+    
 })
 
 module.exports = app;
