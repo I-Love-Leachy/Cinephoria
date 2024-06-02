deafac244dbc82ac304952806f1ba4135c346cd5
Leandra Chevrier <leachevrier@hotmail.com>
2024-06-02 17:54:14 +0200

    Js files modification

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -1,40 +1,54 @@
 const initApp = () => {
-    const hamburgerBtn = document.getElementById('hamburger-button');
-    const mobileMenu = document.getElementById('mobile-menu');
 
-    const toggleMenu = () => {
-        mobileMenu.classList.toggle('hidden');
-        mobileMenu.classList.toggle('flex');
-        hamburgerBtn.classList.toggle('toggle-btn')
-    }
+  const hamburgerBtn = document.getElementById("hamburger-button");
+  const mobileMenu = document.getElementById("mobile-menu");
 
-    hamburgerBtn.addEventListener('click', toggleMenu)
-    mobileMenu.addEventListener('click', toggleMenu)
+  const quickaccess1Btn = document.getElementById("quickaccess-1");
+  const quickaccess2Btn = document.getElementById("quickaccess-2");
 
-    const swiper = new Swiper('.swiper', {
+  const franceCinemaMenu = document.getElementById("france-cinema-list");
+  const belgiumCinemaMenu = document.getElementById("belgium-cinema-list");
 
-        loop: true,
-        autoplay:{
-            delay: 2500,
-        },
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      
-        // And if we need scrollbar
-        scrollbar: {
-          el: '.swiper-scrollbar',
-        },
-      });
+  
+  const openTheaterMenueBtn = document.querySelectorAll('.theater-filter');
+  const theaterMenu = document.getElementById("theater-menu");
+  const closeTheaterMenuBtn = document.getElementById("close-search-theater-menu");
 
-}
+  const toggleHamburgerMenu = () => {
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.classList.toggle("flex");
+    hamburgerBtn.classList.toggle("toggle-btn");
+  };
 
-document.addEventListener('DOMContentLoaded', initApp)
+  const toggleFranceCinemaMenu = () => {
+    franceCinemaMenu.classList.toggle("hidden");
+    franceCinemaMenu.classList.toggle("flex");
+  };
+
+  const toggleBelgiumCinemaMenu = () => {
+    belgiumCinemaMenu.classList.toggle("hidden");
+    belgiumCinemaMenu.classList.toggle("flex");
+  };
+
+  const closeTheaterMenu = () => {
+    theaterMenu.classList.toggle("hidden");
+    theaterMenu.classList.toggle("flex");
+  }
+
+  const openTheaterMenue = () => {
+    theaterMenu.classList.toggle("hidden");
+    theaterMenu.classList.toggle("flex");
+  }
+
+  hamburgerBtn.addEventListener("click", toggleHamburgerMenu);
+  quickaccess1Btn.addEventListener("click", toggleFranceCinemaMenu);
+  quickaccess2Btn.addEventListener("click", toggleBelgiumCinemaMenu);
+  closeTheaterMenuBtn.addEventListener("click", closeTheaterMenu);
+
+  openTheaterMenueBtn.forEach(btn => {
+    btn.addEventListener('click', openTheaterMenue)
+  })
+
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
