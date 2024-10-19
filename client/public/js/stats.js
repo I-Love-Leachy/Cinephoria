98dfa93a411d65b8580fb33e6e4aefd1100ad7eb
I-Love-Leachy <chevrier.devexpert@outlook.com>
2024-10-19 20:42:58 +0200

    fix: bar chart label overlap bug fixed

--- client/public/js/stats.js
+++ client/public/js/stats.js
@@ -1,31 +1,41 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const reservationsData = document.getElementById('reservationsChart').getAttribute('data-reservations');
-    const reservations = JSON.parse(reservationsData);
+document.addEventListener("DOMContentLoaded", () => {
+  const reservationsData = document
+    .getElementById("reservationsChart")
+    .getAttribute("data-reservations");
+  const reservations = JSON.parse(reservationsData);
 
-    const ctx = document.getElementById('reservationsChart').getContext('2d');
+  const ctx = document.getElementById("reservationsChart").getContext("2d");
 
-    const data = {
-        labels: reservations.map(reservation => reservation.title),
-        datasets: [{
-            label: 'Réservations sur la semaine',
-            data: reservations.map(reservation => reservation.count),
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1
-        }]
-    };
+  const data = {
+    labels: reservations.map((reservation) => reservation.title),
+    datasets: [
+      {
+        label: "Réservations sur la semaine",
+        data: reservations.map((reservation) => reservation.count),
+        backgroundColor: "rgba(54, 162, 235, 1)",
+        hoverBackgroundColor: "rgba(227, 176, 75, 1)",
+      },
+    ],
+  };
 
-    const config = {
-        type: 'bar',
-        data: data,
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    };
+  const config = {
+    type: "bar",
+    data: data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+      x: {
+        ticks: {
+          font: {
+            size: 10, // labels font size
+          },
+        },
+      },
+    },
+  };
 
-    new Chart(ctx, config);
+  new Chart(ctx, config);
 });
