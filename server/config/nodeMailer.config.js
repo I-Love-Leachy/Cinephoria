c2437cdfb017ca640035c42f6d33ebcd3d0a51d5
I-Love-Leachy <chevrier.devexpert@outlook.com>
2024-11-01 18:42:01 +0100

    fix: contact form bug fixed 🐛

--- server/config/nodeMailer.config.js
+++ server/config/nodeMailer.config.js
@@ -1,15 +1,15 @@
-require('dotenv').config({path:'../../.env'});
-const nodeMailer = require('nodemailer');
+require("dotenv").config({ path: "../../.env" });
+const nodeMailer = require("nodemailer");
 
 const transporter = nodeMailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-        user: process.env.USER_EMAIL,
-        pass: process.env.USER_PASSWORD
-    }
-})
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "contact.cinephoria@gmail.com",
+    pass: "tslm weil xxeg kbum",
+  },
+});
 
 module.exports = transporter;
--- server/routes/contact/contact.routes.js
+++ server/routes/contact/contact.routes.js
@@ -1,10 +1,44 @@
-const express = require('express');
+const express = require("express");
 const contactRoutes = express.Router();
+const transporter = require("../../config/nodeMailer.config");
+require("dotenv").config({ path: "../../../.env" });
 
-contactRoutes.get('/',(req, res) =>{
-    res.render('layouts/contact', {
-        title: "Contactez-nous."
+contactRoutes.get("/", (req, res) => {
+  res.render("layouts/contact", {
+    title: "Contactez-nous.",
+    message: "",
+  });
+});
+
+contactRoutes.post("/", async (req, res) => {
+  const { email, username, objet, message } = req.body;
+
+  if (!email || !username || !objet || !message) {
+    return res.status(404).send("Fill all input please");
+  }
+
+  const mailOptions = {
+    from: email,
+    replyTo: email,
+    to: "contact.cinephoria@gmail.com",
+    subject: objet,
+    text: `Ce mail vous a été envoyé par ${username} (${email}): ${message}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.render("layouts/contact", {
+      title: "Contactez-nous",
+      message: "Votre message a été envoyé avec succès.",
     });
+  } catch (err) {
+    console.log("Error while sending email", err);
+    res.render("layouts/contact", {
+      title: "Contactez-nous",
+      message:
+        "Une erreur s'est produite lors de l'envoi de votre message. Veuillez réessayer plus tard.",
+    });
+  }
 });
 
 module.exports = contactRoutes;
