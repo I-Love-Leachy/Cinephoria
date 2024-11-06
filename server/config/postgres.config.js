b69d0017e802afb80aa8f8cf9e2b281a1a771789
I-Love-Leachy <chevrier.devexpert@outlook.com>
2024-11-06 12:23:40 +0100

    fix: postgres config for deployment fixed

--- server/config/postgres.config.js
+++ server/config/postgres.config.js
@@ -2,11 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config({ path: "../../.env" });
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: String(process.env.DB_PASSWORD),
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DB_URL,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 10000,
